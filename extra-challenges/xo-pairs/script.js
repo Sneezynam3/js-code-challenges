ef1a07ac5c8eff514dc30f9b51bec11170059a9c
James Snell <vulpes3.1415@yahoo.com>
2024-01-08 11:22:55 -0700

    Added is equal function

--- extra-challenges/xo-pairs/script.js
+++ extra-challenges/xo-pairs/script.js
@@ -10,3 +10,31 @@ xoCheck("zpzpzpp") => true
 xoCheck("zzoo") => false
 
 */
+function hasEqualLetterCounts(word) {
+    const letterCounts = {};
+  
+    // Iterate over each letter in the word
+    for (let i = 0; i < word.length; i++) {
+      const letter = word[i].toLowerCase();
+  
+      // Increase the count for the current letter
+      if (letterCounts[letter]) {
+        letterCounts[letter] += 1;
+      } else {
+        letterCounts[letter] = 1;
+      }
+    }
+  
+    // Get the counts of the letters 'x' and 'o'
+    const xCount = letterCounts['x'] || 0;
+    const oCount = letterCounts['o'] || 0;
+  
+    // Check if the counts of 'x' and 'o' are equal
+    return xCount === oCount;
+  }
+  
+  console.log(hasEqualLetterCounts("ooxx")); // true
+  console.log(hasEqualLetterCounts("xooxx")); // false
+  console.log(hasEqualLetterCounts("ooxXm")); // true
+  console.log(hasEqualLetterCounts("zpzpzpp")); // true
+  console.log(hasEqualLetterCounts("zzoo")); //false
